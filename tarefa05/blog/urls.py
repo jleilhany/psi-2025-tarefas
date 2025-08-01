4d6e748971d8931a10d9ac4069f8982285379bf0
joice <joyceleyllhany@gmail.com>
2025-08-01 16:48:42 -0300

    .

--- tarefa05/blog/urls.py
+++ tarefa05/blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('Post/<int:id>/', views.Lista_posts.html, name='Lista_Posts.html'),
+    path('', views.Lista_posts.html, name='Lista_Posts.html'),
 ]
